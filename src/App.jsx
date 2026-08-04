cc1c157b510e7600bf37b6d57fb02bc554962cfc
Moha <mohammedduale1738@gmail.com>
2026-08-04 21:47:54 +0300

    Fixed App.jsx completely

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,48 @@
-import {useState} from "react";
+import { useState } from "react";
 import TodoList from "./TodoList.jsx";
 import TodoForm from "./TodoForm.jsx";
-import "./index.css"
+import "./index.css";
 
-function App(){
-    const [todos, settodos] = useState([])
+function App() {
+  const [todos, setTodos] = useState([]);
 
-    const addTodo = (text) => {
-        const newTodo = {
-            id:Date.now(),
-            text,
-            completed: false};
-        
-        settodos([...todos, newTodo]);
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Date.now(),
+      text: text,
+      completed: false,
     };
+    setTodos([...todos, newTodo]);
+  };
 
-    const deleteTodo = (id)=>{
-        setTodos(todos.filter(todo=> todo.id !== id));
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
-    };
-    
-    const toggleTodo = (id) =>{
-        settodos(todos.map(todo =>
-            todo.id === id ?{...todo, completed: !todo.completed}:todo
-        ));
-    };
-
-    return (
-        <div className="app-container">
-            <h1 className="app-title"> Todo List</h1>
-            <TodoForm onAddTodo={addTodo}/>
-            <Todolist todos={todos} onDelete={deleteTodo} onToggle={toggleTodo}/>
-            <div className="todo-footer">{todos.length} items</div>
-        </div>
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      )
     );
+  };
+
+  return (
+    <div className="app-container">
+      <h1 className="app-title">📋 Todo List</h1>
+      <TodoForm onAddTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        onDelete={deleteTodo}
+        onToggle={toggleTodo}
+      />
+      <div className="todo-footer">
+        {todos.length} item{todos.length !== 1 ? "s" : ""}
+      </div>
+    </div>
+  );
 }
+
 export default App;
